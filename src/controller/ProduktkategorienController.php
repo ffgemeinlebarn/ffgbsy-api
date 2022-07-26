ac5492a6cb3f3cbb004333a9a443796c7ed6a717
Jakob Vesely <dev@jakob.fyi>
2022-07-26 22:06:25 +0200

    Produktkategorien

---
+++ src/controller/ProduktkategorienController.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FFGBSY\Controller;
+
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use FFGBSY\Services\ProduktkategorienService;
+
+final class ProduktkategorienController extends BaseController
+{
+    private ProduktkategorienService $produktkategorienService;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->produktkategorienService = $container->get('produktkategorien');
+        $this->produktbereicheService = $container->get('produktbereiche');
+    }
+
+    public function create(Request $request, Response $response): Response
+    {
+        $data = $this->produktkategorienService->create($request->getParsedBody());
+        return $this->responseAsJson($response, $data);
+    }
+
+    public function readAll(Request $request, Response $response): Response
+    {
+        $this->request = $request;
+        $data = $this->produktkategorienService->read();
+        return $this->responseAsJson($response, $data);
+    }
+
+    public function readSingle(Request $request, Response $response, array $args): Response
+    {
+        $this->request = $request;
+        $data = $this->produktkategorienService->read($args['id']);
+        $data->produktbereich = $this->produktbereicheService->read($data->produktbereiche_id);
+        return $this->responseAsJson($response, $data);
+    }
+
+    public function update(Request $request, Response $response): Response
+    {
+        $this->request = $request;
+        $data = $this->produktkategorienService->update($request->getParsedBody());
+        $data->produktbereich = $this->produktbereicheService->read($data->produktbereiche_id);
+        return $this->responseAsJson($response, $data);
+    }
+
+    public function delete(Request $request, Response $response, array $args): Response
+    {
+        $this->request = $request;
+        $data = $this->produktkategorienService->delete($args['id']);
+        return $this->responseAsJson($response, $data);
+    }
+}
--- src/services/ProduktkategorienService.php
+++ src/services/ProduktkategorienService.php
@@ -10,5 +10,63 @@
 
     final class ProduktkategorienService extends BaseService
     {
-        
+        public function create($data)
+        {
+            $sth = $this->db->prepare("INSERT INTO produktkategorien (name, color, produktbereiche_id, drucker_id_level_1, sortierindex) VALUES (:name, :color, :produktbereiche_id, :drucker_id_level_1, :sortierindex)");
+            $sth->bindParam(':name', $data['name'], PDO::PARAM_STR);
+            $sth->bindParam(':color', $data['color'], PDO::PARAM_STR);
+            $sth->bindParam(':produktbereiche_id', $data['produktbereich']['id'], PDO::PARAM_INT);
+            $sth->bindParam(':drucker_id_level_1', $data['drucker_id_level_1'], PDO::PARAM_INT);
+            $sth->bindParam(':sortierindex', $data['sortierindex'], PDO::PARAM_INT);
+            $sth->execute();
+
+            return $this->read($this->db->lastInsertId());
+        }
+
+        public function read($id = null)
+        {
+            if ($id != null)
+            {
+                $sth = $this->db->prepare("SELECT * FROM produktkategorien WHERE id=:id");
+                $sth->bindParam(':id', $id, PDO::PARAM_INT);
+                return $this->singleRead($sth);
+            }
+            else
+            {
+                $sth = $this->db->prepare("SELECT * FROM produktkategorien");
+                return $this->multiRead($sth);
+            }
+        }
+
+        public function update($data)
+        {
+            $sth = $this->db->prepare("UPDATE produktkategorien SET name=:name, color=:color, produktbereiche_id=:produktbereiche_id, drucker_id_level_1=:drucker_id_level_1, sortierindex=:sortierindex WHERE id=:id");
+            $sth->bindParam(':id', $data['id'], PDO::PARAM_INT);
+            $sth->bindParam(':name', $data['name'], PDO::PARAM_STR);
+            $sth->bindParam(':color', $data['color'], PDO::PARAM_STR);
+            $sth->bindParam(':produktbereiche_id', $data['produktbereich']['id'], PDO::PARAM_INT);
+            $sth->bindParam(':drucker_id_level_1', $data['drucker_id_level_1'], PDO::PARAM_INT);
+            $sth->bindParam(':sortierindex', $data['sortierindex'], PDO::PARAM_INT);
+            $sth->execute();
+            
+            return $this->read($data['id']);
+        }
+
+        public function delete($id)
+        {
+            $sth = $this->db->prepare("DELETE FROM produktkategorien WHERE id = :id");
+            $sth->bindParam(':id', $id, PDO::PARAM_INT);
+            $result = $sth->execute();
+            
+            return $result;
+        }
+
+        protected function singleMap($obj)
+        {
+            $obj->id = $this->asNumber($obj->id);
+            $obj->drucker_id_level_1 = $this->asNumberOrNull($obj->drucker_id_level_1);
+            $obj->produktbereiche_id = $this->asNumber($obj->produktbereiche_id);
+            $obj->sortierindex = $this->asNumber($obj->sortierindex);
+            return $obj;
+        }
     }
